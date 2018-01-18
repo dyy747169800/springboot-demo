4ceb1189e31576fe7fa9e86d5da7f9e60cad88bf
段杨宇 <duanyy@jowto.com>
2018-01-18 13:46:15 +0800

    修改

--- src/main/java/com/dyy/web/RestDemoController.java
+++ src/main/java/com/dyy/web/RestDemoController.java
@@ -4,7 +4,9 @@ import com.dyy.activemq.producer.JMSProducer;
 import com.dyy.constant.Constant;
 import org.apache.activemq.command.ActiveMQQueue;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Required;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
